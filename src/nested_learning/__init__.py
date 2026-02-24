550564a9ef9fa593ec6806f57b00a4dfa26840b6
kmccleary3301 <kmccl24@lsu.edu>
2026-02-24 16:08:50 -0600

    Bump package version to 0.2.0

--- src/nested_learning/__init__.py
+++ src/nested_learning/__init__.py
@@ -7,6 +7,6 @@ from .levels import LevelClock, LevelSpec  # noqa: F401
 try:
     __version__ = version("nested-learning")
 except PackageNotFoundError:  # pragma: no cover - editable/local source tree
-    __version__ = "0.2.0rc2"
+    __version__ = "0.2.0"
 
 __all__ = ["LevelClock", "LevelSpec", "__version__"]
